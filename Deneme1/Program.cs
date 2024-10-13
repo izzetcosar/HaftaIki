26774a748d853a15f19fa7994b7f8648b8f1c299
izzetcosar <izzetcosar@gmail.com>
2024-10-14 02:06:37 +0300

    ilk methot uygulamamız

---
+++ Deneme1/Program.cs
@@ -0,0 +1,7 @@
+﻿static int Carpim(int x, int y) // Parametre olarak alınan x ve y'nin çarpımını geri döndürüyoruz
+{
+    return x * y;
+}
+
+int sonuc = Carpim(3, 5); // Carpim metodunu çağırıyoruz ve 3 ile 5'in çarpımını hesaplıyoruz
+Console.WriteLine("Çarpım: " + sonuc); //Çarpım sonucunu ekrana yazdırıyoruz
---
+++ Pratik-IlkMetotUygulamamız/Program.cs
@@ -0,0 +1,52 @@
+﻿#region Geriye Değer Döndürmeyen Bir void metot
+static void sarkısozu()
+{
+    Console.WriteLine("Fısılda ismimi rüzgarla,doldur yelkenlerimi");
+
+}
+sarkısozu();
+Console.ReadKey(true);
+#endregion
+#region Geriye Tamsayı Döndüren Bir metot
+static int Rastagelekalan()
+
+{
+    Random rnd = new Random();
+    int sayi = rnd.Next();
+    int sayidankalan = sayi % 2;
+    return sayidankalan;
+
+
+}
+int rastgele = Rastagelekalan();
+
+Console.WriteLine(rastgele);
+Console.ReadKey(true);
+
+#endregion
+#region Parametre Alan ve Geriye Değer Döndüren Bir Metot
+
+static int carp(int a, int b)
+{
+    return a * b;
+}
+
+int carpim = carp(3, 5);
+
+Console.WriteLine("Cevap: " + carpim);
+Console.ReadKey(true);
+
+
+#endregion
+#region Parametre Alan ve Geriye Değer Döndürmeyen bir Metot
+
+static void Selamlama(string ad, string soyad)
+{
+    Console.WriteLine($"Hoşgeldin " + ad + " " + soyad);
+
+}
+Selamlama("İzzet", "Çoşar");
+
+Console.ReadKey(true);
+#endregion
+
